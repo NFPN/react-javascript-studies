6546f1ac266af492201472450759953f12a3da9f
Natanael Nunes / MadWallker <natanaelfpnunes@gmail.com>
2023-11-10 09:59:52 -0300

    add: popup add & delete cities

--- weather-chrome-extension/src/popup/popup.tsx
+++ weather-chrome-extension/src/popup/popup.tsx
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import { Add as AddIcon } from "@material-ui/icons";
 import "./popup.css";
 import WeatherCard from "./WeatherCard";
 import "fontsource-roboto";
+import { Box, Grid, IconButton, InputBase, Paper } from "@material-ui/core";
 
 const App: React.FC<{}> = () => {
+    const [cities, setCities] = useState<string[]>([
+        "Sao Jose do Rio Preto",
+        "Sao Paulo",
+        "ERROR",
+    ]);
+    const [cityInput, setCityInput] = useState<string>("");
+
+    const handleCityButtonClick = () => {
+        if (cityInput === "") {
+            return;
+        }
+        setCities([...cities, cityInput]);
+        setCityInput("");
+    };
+
+    const handleCityDeleteButoonClick = (index: number) => {
+        cities.splice(index, 1);
+        setCities([...cities]);
+    };
+
     return (
-        <div>
-            <WeatherCard city="Sao Jose do Rio Preto" />
-            <WeatherCard city="Sao Paulo" />
-            <WeatherCard city="error" />
-        </div>
+        <Box mx="8px" my="16px">
+            <Grid container>
+                <Grid item>
+                    <Paper>
+                        <Box px="15px" py="5px">
+                            <InputBase
+                                placeholder="Add a city name"
+                                value={cityInput}
+                                onChange={(event) =>
+                                    setCityInput(event.target.value)
+                                }
+                            />
+                            <IconButton onClick={handleCityButtonClick}>
+                                <AddIcon />
+                            </IconButton>
+                        </Box>
+                    </Paper>
+                </Grid>
+            </Grid>
+            {cities.map((city, index) => (
+                <WeatherCard
+                    city={city}
+                    key={index}
+                    onDelete={() => handleCityDeleteButoonClick(index)}
+                />
+            ))}
+            <Box height="16px"></Box>
+        </Box>
     );
 };
 
